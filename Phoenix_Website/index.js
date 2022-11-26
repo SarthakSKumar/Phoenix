a74bc746c231886e6660acff0618e22f4331ac27
Sarthak S Kumar <sskworld9742@gmail.com>
2022-11-26 22:35:10 +0530

    Added Middleware for Entry Page Static Files

--- Phoenix_Website/index.js
+++ Phoenix_Website/index.js
@@ -4,9 +4,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+//Entry Page
+app.use("/css", express.static(__dirname + '/Entry Page/css'));
+app.use("/images", express.static(__dirname + '/Entry Page/images'));
+app.use("/js", express.static(__dirname + '/Entry Page/js'));
+app.use("/plugins", express.static(__dirname + '/Entry Page/plugins'));
+
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/Entry Page/index.html'));
 });
 
 app.listen(port);
