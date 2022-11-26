b3b043ec6f0eaa2ac775f916fc010a665959d98e
Sarthak S Kumar <sskworld9742@gmail.com>
2022-11-26 22:37:01 +0530

    Handle GET Requests with Express

--- Phoenix_Website/index.js
+++ Phoenix_Website/index.js
@@ -15,5 +15,15 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/Entry Page/index.html'));
 });
 
+app.get('/home', function (req, res) {
+    res.sendFile(path.join(__dirname, '/Entry Page/index.html'));
+});
+
+app.get('/order', function (req, res) {
+    res.sendFile(path.join(__dirname, '/Entry Page/index.html'));
+});
+
+
+
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
