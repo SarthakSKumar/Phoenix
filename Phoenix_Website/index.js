755b0691eeb1a53cdf92fffdc99a3ca7fa2d4101
Sarthak S Kumar <sskworld9742@gmail.com>
2022-11-26 22:51:21 +0530

    Added Middleware functions for E-Commerce Page

--- Phoenix_Website/index.js
+++ Phoenix_Website/index.js
@@ -10,6 +10,11 @@ app.use("/images", express.static(__dirname + '/Entry Page/images'));
 app.use("/js", express.static(__dirname + '/Entry Page/js'));
 app.use("/plugins", express.static(__dirname + '/Entry Page/plugins'));
 
+//E-Commerce Page
+app.use("/css", express.static(__dirname + '/Entry Page/css'));
+app.use("/images", express.static(__dirname + '/Entry Page/images'));
+app.use("/js", express.static(__dirname + '/Entry Page/js'));
+app.use("/plugins", express.static(__dirname + '/Entry Page/plugins'));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/Entry Page/index.html'));
